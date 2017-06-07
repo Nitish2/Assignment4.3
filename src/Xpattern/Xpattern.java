acf0be35afd4433cc2793d1e8698434149d3b9b8
Nitish2 <nitishn886@gmail.com>
2017-06-07 15:39:46 +0530

    Assignment4.3

---
+++ src/Xpattern/Xpattern.java
@@ -0,0 +1,24 @@
+package Xpattern;
+
+public class Xpattern {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i, j;
+		for (i = 1; i <= 5; i++)
+		{
+			for (j = 1; j <= 5; j++) {
+				if (j == i || j == 6 - i)
+				{
+					System.out.print("*");
+						
+                } 
+			else {
+					System.out.print(" ");
+				}
+			}
+			System.out.println();
+		}
+
+	}
+}
